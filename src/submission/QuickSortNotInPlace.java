4db2be6b93ae9a770e1923c00945c81b75b7c907
moesdough <moritz.lison@gmx.de>
2022-12-14 22:27:22 +0100

    Not in Place

--- src/submission/QuickSortNotInPlace.java
+++ src/submission/QuickSortNotInPlace.java
@@ -2,34 +2,32 @@ package submission;
 import java.util.ArrayList;
 import java.util.List;
 public class QuickSortNotInPlace {
+public static List<Integer> sort(List<Integer> list) {
+    if (list.size() <= 1) {
+        return list;
+    }
 
+    int pivot = list.get(0);
+    List<Integer> lesser = new ArrayList<>();
+    List<Integer> greater = new ArrayList<>();
+    List<Integer> equal = new ArrayList<>();
 
-        public static List<Integer> sort(List<Integer> list) {
-            if (list.size() <= 1) {
-                return list;
-            }
-
-            int pivot = list.get(list.size() / 2);
-            List<Integer> lesser = new ArrayList<>();
-            List<Integer> greater = new ArrayList<>();
-            List<Integer> equal = new ArrayList<>();
-
-            for (int i : list) {
-                if (i < pivot) {
-                    lesser.add(i);
-                } else if (i > pivot) {
-                    greater.add(i);
-                } else {
-                    equal.add(i);
-                }
-            }
-
-            List<Integer> sorted = sort(lesser);
-            sorted.addAll(equal);
-            sorted.addAll(sort(greater));
-
-            return sorted;
+    for (int i : list) {
+        if (i < pivot) {
+            lesser.add(i);
+        } else if (i > pivot) {
+            greater.add(i);
+        } else {
+            equal.add(i);
         }
+    }
+
+    List<Integer> sorted = sort(lesser);
+    sorted.addAll(equal);
+    sorted.addAll(sort(greater));
+
+    return sorted;
+         }
     }
 
 
